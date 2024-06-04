e216b381006e9e4f4aa80cc34837adf9eb6ca658
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-06-04 23:44:03 +0700

    bt doi mau bg

--- index.js
+++ index.js
@@ -13,31 +13,13 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 io.on('connection', (socket) => {
-  console.log('Co nguoi ket noi: ' + socket.id);
+  // console.log("Co nguoi ket noi: " + socket.id);
 
-  socket.on('disconnect', () => {
-    console.log(socket.id + ' ngat ket noi');
+  socket.on('CLIENT-SEND-MAU', (data) => {
+    // console.log(data);
+    io.sockets.emit('SERVER-SEND-MAU', data);
   });
 
-  socket.on('client-send-data', (data) => {
-    console.log(socket.id + ' gui: ' + data);
-
-    //truong hop 1:  1 client gui data len server, server nhan duoc data va gui lai cho tat ca client khac
-    //sau khi nhan duoc tin hieu, server gui lai data
-    io.sockets.emit('server-send-data', data);
-
-
-    //truong hop 2: client gui data len server, server nhan duoc data va gui lai cho chinh client do
-    socket.emit('server-send-data', data);
-
-    //truong hop 3: client gui data len server, server nhan duoc data va gui lai cho tat ca client khac ngoai client do
-    socket.broadcast.emit('server-send-data', data);
-
-    //truong hop 4: client gui data len server, server nhan va gui lai cho nhung client co id la 1, 2, 3
-    //moc id cua client la socket.id
-    //chat rieng: io.to(socketId).emit('message', 'for your eyes only');
-    // socket.to('1').emit('server-send-data', data);
-  });
 });
 
 app.get('/', (req, res) => {
